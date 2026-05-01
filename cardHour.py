051bcf966a0186b2b7ed8a71e78b0755df863816
Dylan Lewis <65700772+GarkLlygad@users.noreply.github.com>
2026-04-30 21:43:29 -0700

    Added scanner pages and soup basics

--- cardHour.py
+++ cardHour.py
@@ -1,22 +1,23 @@
 from internetarchive import get_item, search_items
 import json
 from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
 
 cardCountTotal = 0
 uploader = 'associate-dylan-lewis'
-print("Current selected uploader is: " + uploader)
+# print("Current selected uploader is: " + uploader)
 
 currentDay = datetime.now()
 currentDayComplete = currentDay.strftime("%Y%m%d") + "000000"
 nextDay = currentDay + timedelta(days=1)
 nextDayComplete = nextDay.strftime("%Y%m%d") + "000000"
-print(currentDayComplete, nextDayComplete)
+# print(currentDayComplete, nextDayComplete)
 
 allItems = set()
 for i in search_items(f'uploader:{uploader} scandate:[{currentDayComplete} TO {nextDayComplete}]'):
     allItems.add(i['identifier'])
 itemCountTotal = len(allItems)
-print(itemCountTotal)
+# print(itemCountTotal)
 
 for i in range(1, 20):
     items = set()
@@ -25,7 +26,7 @@ for i in range(1, 20):
         allItems.discard(j['identifier'])
     cardCountTotal += (len(items) * i)
 
-print("Currently at " + str(cardCountTotal))
+# print("Currently at " + str(cardCountTotal))
 
 if len(allItems) > 0:
     print("Found " + str(len(allItems)) + " additional items.")
@@ -35,8 +36,14 @@ if len(allItems) > 0:
     else:
         print("No large items found")
 
-print("Card count so far: " + str(cardCountTotal))
-print("Item count so far: " + str(itemCountTotal))
+# print("Card count so far: " + str(cardCountTotal))
+# print("Item count so far: " + str(itemCountTotal))
+
+with open("scanners/dylan.html") as fp:
+    soup = BeautifulSoup(fp, 'html.parser')
+    soup.find(id="DylanToday").string = str(cardCountTotal)
+with open("scanners/dylan.html", "w") as fp:
+    fp.write(soup.prettify())
 
 # filename = "test_" + datetime.now().strftime("%H%M%S_%Y%m%d") + ".json"
 # with open(filename, "w") as file:
