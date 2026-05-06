34f1390c1eff31d7efc54de7dd0e65621a4542b4
llygadpi <lewis.dylan.m@gmail.com>
2026-05-05 22:49:01 -0700

    RaspberryPi auto update 2026-05-05 22:49:01

--- autoRun.sh
+++ autoRun.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-git fetch
-git status
-git commit -am "Bash test automation $(date +'%Y-%m-%d %H:%M:%S')"
-git push
+cd /home/gark/MicroficheScanning/
+if [[ -z $(git status -s) ]]; then
+  echo "No changes to commit."
+  exit 0
+fi
+git add .
+git commit -m "RaspberryPi auto update $(date +'%Y-%m-%d %H:%M:%S')"
+git push -u origin master
