e4fd0c90c2f63b00ff473561eae17ce33412dfdd
llygadpi <lewis.dylan.m@gmail.com>
2026-08-02 20:29:36 -0700

    Added start of day clearing support

---
+++ clear.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+
+uploaders = ['dylan', 'elijah', 'fernando', 'axia', 'cameron', 'danielle', 'emmie', 'fredy', 'hope', 'jay', 'josh', 'kurt', 'maeve', 'maya', 'michael', 'miguel', 'sophia', 'sylvie', 'vinny', 'xane']
+
+for uploaderArg in uploaders:
+    with open(f"/home/gark/MicroficheScanning/scanners/{uploaderArg}.html") as fp:
+        soup = BeautifulSoup(fp, 'html.parser')
+        soup.find(id=f"{uploaderArg}Today").string = str(0)
+    with open(f"/home/gark/MicroficheScanning/scanners/{uploaderArg}.html", "w") as fp:
+        fp.write(soup.prettify())
